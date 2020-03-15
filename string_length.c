363293b1378eed94e98619cba80f4dd6b336d211
Kelsie Merchant <eislek02@gmail.com>
2020-03-14 23:18:51 -0500

    Add C function code to determine length of string

---
+++ string_length.c
@@ -0,0 +1,15 @@
+/**
+ * string_length - determines the length of a string
+ * @s: input character string to determine length of
+ *
+ * Return: integer count of length
+ */
+
+int string_length(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i]; i++)
+		continue;
+	return(i);
+}
