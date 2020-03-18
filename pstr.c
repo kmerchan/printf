9f373cb3f0a20d18c4d67c3ccdcb1544fddc3161
shontrell <shontrell.scott3@gmail.com>
2020-03-18 04:02:07 +0000

    Added lines to file

--- pstr.c
+++ pstr.c
@@ -7,9 +7,15 @@
  */
 int print_string(va_list ap)
 {
-	char *x = va_arg(ap, char*);
+	char *x = va_arg(ap, char*) *nil_string = "(null)";
 	int y, len = string_length(x);
 
+	if (x == NULL)
+	{
+		for (y = 0; nil_string[y]; y++)
+			_putchar(nil_string[y]);
+		return (-1);
+	}
 	for (y = 0; y < len ; y++)
 		_putchar(x[y]);
 	return (len - 1);
